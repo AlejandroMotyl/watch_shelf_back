f8b8293d8fbec6d2f2e4d8b8d906d888b1e32d82
AlejandroMotyl <Alejandromotylwrk@gmail.com>
2026-09-06 17:54:23 +0200

    updating user image

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,8 @@
 import type { NextFunction, Request, Response } from 'express';
 import createHttpError from 'http-errors';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { pool } from '../config/db.js';
+import type { UploadApiResponse } from 'cloudinary';
 
 export const getCurrentUser = async (
   req: Request,
@@ -18,5 +21,39 @@ export const getCurrentUser = async (
     });
   } catch (error) {
     next(error);
+  }
+};
+
+export const updateUserAvatar = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  // ! Add clearing out the previous image from cloudinary
+  try {
+    if (!req.file) {
+      next(createHttpError(400, 'No file'));
+      return;
+    }
+
+    const result = (await saveFileToCloudinary(
+      req.file.buffer,
+    )) as UploadApiResponse;
+    console.log('CLOUDINARY RESULT:', result);
+
+    const userData = await pool.query(
+      `
+        UPDATE users
+        SET avatar_url = $1
+        WHERE id = $2
+        RETURNING id, username, email, avatar_url, created_at
+      `,
+      [result.secure_url, req.user!.id],
+    );
+
+    res.status(200).json({ user: userData.rows[0] });
+  } catch (err) {
+    console.error('updateUserAvatar failed:', err);
+    next(err instanceof Error ? err : new Error(JSON.stringify(err)));
   }
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
-import { getCurrentUser } from '../controllers/userController.js';
+import {
+  getCurrentUser,
+  updateUserAvatar,
+} from '../controllers/userController.js';
+import { upload } from '../middleware/multer.js';
 
 const router = Router();
 
 router.get('/profile', authenticate, getCurrentUser);
+router.patch(
+  '/profile',
+  authenticate,
+  upload.single('avatar'),
+  updateUserAvatar,
+);
 // router.get('/profile/reviews');
 // router.get('/profile/favorites');
 // router.get('/profile/history');
